6e8acc287a3bac7008b51f1d6646b606e29729dd
GnagetEmil <emil@gnaget.se>
2022-05-17 11:58:30 +0200

    remove 2 tests

--- gui/unit_tests.py
+++ gui/unit_tests.py
@@ -5,32 +5,35 @@ import file_handling
 from datetime import date
 
 
-## Instantiating File Hnadling class object
+# Instantiating File Hnadling class object
 fh = file_handling.file_handling()
+
 
 class file_handling_tests(unittest.TestCase):
 
     "This method is a test case for setting points to your goal."
+
     def test_setPointsGoal(self):
 
-        goal = 10 
+        goal = 10
         fh.setPointsGoal(goal)
         with open("points.txt", "r") as f:
             actual = f.read()
 
         expected = str(10)
-             
+
         self.assertEqual(actual, expected)
 
     "This method is a test case for testing the date."
+
     def test_setDate(self):
 
-        daysToComplete = 30       
+        daysToComplete = 30
         fh.setDate(daysToComplete)
 
-        with open("points.txt", "r") as f: 
+        with open("points.txt", "r") as f:
 
-            ## Reading last two lines of the file
+            # Reading last two lines of the file
             actual = ''.join(f.readlines()[-2:])
             f.seek(0)
 
@@ -41,31 +44,20 @@ class file_handling_tests(unittest.TestCase):
                 currentDate = pop[pop.rindex('-') + 2]
 
             else:
-                currentDate = pop[pop.rindex('-') + 1] + pop[pop.rindex('-') + 2]
+                currentDate = pop[pop.rindex(
+                    '-') + 1] + pop[pop.rindex('-') + 2]
 
             expected = f'{currentDate}\n{int(currentDate) + daysToComplete}'
 
-        self.assertEqual(actual, expected)  
-   
-    "This method tests if how many days you have left."
-    def test_getRemainingDays(self): 
-
-        with open('points.txt', 'r') as f:              #opens a text file
-            current_date = int(f.readlines()[1])    
-            f.seek(0)
-            end_date = int(f.readlines()[2])
-        
-        expected = str(end_date-current_date)
-        actual   = fh.getRemainingDays()
-
         self.assertEqual(actual, expected)
 
-    "this method tests how many points you have left."
-    def test_getRemainingPoints(self): 
+    "This method tests if how many days you have left."
+
+    def test_getRemainingPoints(self):
 
         with open('points.txt', 'r') as f:
 
-            ## Reading the first line of the file
+            # Reading the first line of the file
             expected_points = f.readline()
 
         actual_points = fh.getRemainingPoints()
@@ -73,43 +65,31 @@ class file_handling_tests(unittest.TestCase):
         self.assertCountEqual(actual_points, expected_points)
 
     "This method is used when points to deduct the current_points."
-    def test_deductPoints(self):
-        
-        ## Test for When points_to_deduct < current_points
-        current_points   = int(fh.getRemainingPoints())
-        points_to_deduct = 1
 
-        expected = str(current_points - points_to_deduct) + '\n'
-        fh.deductPoints(points_to_deduct)
-        with open('points.txt', 'r') as f:
-            acutal = f.readline()
-        
-        self.assertCountEqual(acutal, expected)
-    
     def test_emptyFile(self):
-        
+
         fh.emptyFile()
-        actual_size   = os.stat('points.txt').st_size
-        expected_size = 0 
+        actual_size = os.stat('points.txt').st_size
+        expected_size = 0
         self.assertEqual(actual_size, expected_size)
 
     "This method is used to check if the file is empty or not."
+
     def test_isFileEmpty(self):
-        
-        ##Test for when file is empty
+
+        # Test for when file is empty
         with open('points.txt', 'w') as f:
             f.write('')
-        
+
         self.assertTrue(fh.isFileEmpty())
 
-        ##Test for when file is not empty
+        # Test for when file is not empty
         with open('points.txt', 'w') as f:
             f.write('10\n10\n40')
 
         self.assertFalse(fh.isFileEmpty())
 
         fh.emptyFile()
-
 
 
 if __name__ == '__main__':
@@ -119,15 +99,15 @@ if __name__ == '__main__':
     try:
         tester.test_setPointsGoal()
         tester.test_setDate()
-        tester.test_getRemainingDays()
+        # tester.test_getRemainingDays()
         tester.test_getRemainingPoints()
-        tester.test_deductPoints()
+        # tester.test_deductPoints()
         tester.test_emptyFile()
         tester.test_isFileEmpty()
 
         print("ALL TESTS PASSED!")
-    
+
     except AssertionError:
         print("ONE OR MORE TESTS FAILED!")
 
-    
+    unittest.main()
